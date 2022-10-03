5a7199a7cf09760cf449e8711c69731691eba399
reesericci <me@reeseric.ci>
2022-10-03 14:40:12 -0500

    promises be like

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -48,58 +48,66 @@ export const actions = {
         })) {
             return invalid(400, {...obj, emailTaken: true})
         }
-        await prisma.participant.create({
+        const db = await prisma.participant.create({
             data: {
                 ...obj
             }
         })
         
 
-        client.sendEmail({
-        "From": "reese@hackaustin.net",
-        "To": obj.email,
-        "Subject": "You're registered for Hack Austin!",
-        "TextBody": `
-            Hi ${obj.name}!
-
-            You're set to attend Hack Austin, the first hackathon for Austin high-schoolers since the pandemic!
-            
-            We'll reach back out soon about waivers, proof of vaccination and more, so stay tuned.
-
-            
-            We can't wait to see you at Hack Austin!
-            
-            --reese
-
-            -----   
-            We value your privacy and we'll never track whether you read or open our emails.
-        `,
-        "MessageStream": "outbound"
-        });
-
-        client.sendEmail({
-            "From": "team@hackaustin.net",
-            "To": "reese@hackaustin.net",
-            "Subject": `${obj.name} has just registered for Hack Austin!`,
-            "TextBody": `
-                ${obj.name} has just registered for HA with details:
-
-                Name: ${obj.name}
-                Pronouns: ${obj.pronouns}
-                Email: ${obj.email}
-                Date of birth: ${obj.dob}
-                T-shirt size: ${obj.tShirtSize}
-                Technical skill: ${obj.technicalSkill}
-                Dietary restrictions: ${obj.dietaryRestrictions}
 
 
-                They${obj.workshop ? "" : " don't"} want to host a workshop
+        const confirmationEmailPromise = new Promise((resolve) => {
+            client.sendEmail({
+                "From": "reese@hackaustin.net",
+                "To": obj.email,
+                "Subject": "You're registered for Hack Austin!",
+                "TextBody": `
+                    Hi ${obj.name}!
+    
+                    You're set to attend Hack Austin, the first hackathon for Austin high-schoolers since the pandemic!
+                    
+                    We'll reach back out soon about waivers, proof of vaccination and more, so stay tuned.
+    
+                    
+                    We can't wait to see you at Hack Austin!
+                    
+                    --reese
+    
+                    -----   
+                    We value your privacy and we'll never track whether you read or open our emails.
+                `,
+                "MessageStream": "outbound"
+            }, resolve);
+        })
 
+        const notificationEmailPromise = new Promise((resolve) => {
+            client.sendEmail({
+                "From": "team@hackaustin.net",
+                "To": "reese@hackaustin.net",
+                "Subject": `${obj.name} has just registered for Hack Austin!`,
+                "TextBody": `
+                    ${obj.name} has just registered for HA with details:
+    
+                    Name: ${obj.name}
+                    Pronouns: ${obj.pronouns}
+                    Email: ${obj.email}
+                    Date of birth: ${obj.dob}
+                    T-shirt size: ${obj.tShirtSize}
+                    Technical skill: ${obj.technicalSkill}
+                    Dietary restrictions: ${obj.dietaryRestrictions}
+    
+    
+                    They${obj.workshop ? "" : " don't"} want to host a workshop
+    
+    
+                `,
+                "MessageStream": "outbound"
+            }, resolve);
+        })
 
-            `,
-            "MessageStream": "outbound"
-        });
-        console.log(client)
-        throw redirect(302, "/success")
+        await Promise.all([confirmationEmailPromise,notificationEmailPromise]).then(() => {
+            throw redirect(302, "/success")
+        })
     }
 };
